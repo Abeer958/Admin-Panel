b817423f5b2738a285d7da76f1ba284a7791cdcf
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-07-24 14:09:48 +0400

    Fixed Id back to any

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -27,12 +27,12 @@ function App() {
         <Route path="login" element={<Login />}/>
         <Route path="users">
           <Route index element={<List />}/>
-          <Route path=":123" element={<Single />}/>
+          <Route path=":userId" element={<Single />}/>
           <Route path="new" element={<New inputs ={userInputs} title="Add New User" />}/>
         </Route>
         <Route path="products">
           <Route index element={<List />}/>
-          <Route path=":123" element={<Single />}/>
+          <Route path=":productId" element={<Single />}/>
           <Route path="new" element={<New inputs={productInputs} title="Add New Product" />}/>
         </Route>
         
